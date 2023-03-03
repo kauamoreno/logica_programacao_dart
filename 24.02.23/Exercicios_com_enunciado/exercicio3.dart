e0cc2910d205d456c5f54d85548228131787fd7a
Aluno <kaua.moreno2005@gmail.com>
2023-03-03 08:11:03 -0300

    finalização Exercicios_com_enunciado/exercicio3.dart

--- 24.02.23/Exercicios_com_enunciado/exercicio3.dart
+++ 24.02.23/Exercicios_com_enunciado/exercicio3.dart
@@ -3,3 +3,18 @@ Faça um programa que leia o raio de uma circunferência.
 Calcule e mostre na tela sua área através da fórmula:
 A = PI * r^2
 */
+
+import 'dart:io';
+import 'dart:math';
+
+var raioEsfera;
+void main(){
+  print("Digite o raio da esfera: ");
+  raioEsfera = stdin.readLineSync();
+
+  double r = double.parse(raioEsfera);
+
+  double area = pi * pow(r, 2);
+
+  print("A área de circunferência é de $area"); 
+}
