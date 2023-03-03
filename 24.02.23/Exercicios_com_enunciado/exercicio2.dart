b68a92418b8c2ce6aadad4750d79bebf4b31104c
Aluno <kaua.moreno2005@gmail.com>
2023-03-03 08:10:10 -0300

    finalização Exercicios_com_enunciado/exercicio2.dart

--- 24.02.23/Exercicios_com_enunciado/exercicio2.dart
+++ 24.02.23/Exercicios_com_enunciado/exercicio2.dart
@@ -3,3 +3,17 @@ Faça um programa que leia o raio de uma esfera.
 Calcule e mostre seu volume na tela através da fórmula:
 V = 4/3 * PI * r^3
 */
+
+import 'dart:io';
+import 'dart:math';
+
+var raioEsfera;
+void main(){
+  print("Digite o raio da esfera: ");
+  raioEsfera = stdin.readLineSync();
+
+  double r = double.parse(raioEsfera);
+  double volumeEsfera = (4/3) * pi * pow(r, 3);
+
+  print("O volume da esfera é de $volumeEsfera");
+}
