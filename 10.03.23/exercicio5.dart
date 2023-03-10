d7e9380551727bbdba177896526bcd5109eaeebf
Aluno <kaua.moreno2005@gmail.com>
2023-03-10 10:33:20 -0300

    add exercicio5 dia 10.03.23

---
+++ 10.03.23/exercicio5.dart
@@ -0,0 +1,44 @@
+/*
+Um funcionário irá receber um aumento de acordo com o seu plano de trabalho, conforme a tabela.
+Faça um programa que leia o plano de trabalho e o salário atual de um funcionário. 
+Em seguida, calcule e mostre na tela o valor do reajuste e o
+salário reajustado.
+
+Plano     Aumento
+A         10%
+B         15%
+C         20%
+*/
+
+import 'dart:io';
+
+var planoDeTrabalho;
+var salarioAtualS;
+void main() {
+  double aumento = 0;
+  
+  print("Plano de trabalho: (A) (B) (C)");
+  planoDeTrabalho = stdin.readLineSync();
+
+  switch (planoDeTrabalho.toUpperCase()){
+    case 'A':{
+      aumento = 1.1;
+      break;
+    }
+    case 'B':{
+      aumento = 1.15;
+      break;
+    }
+    case 'C':{
+      aumento = 1.2;
+      break;
+    }
+  }
+
+  print("Salario atual: ");
+  salarioAtualS = stdin.readLineSync();
+  double salarioAtualD = double.parse(salarioAtualS);
+  
+  double reajuste = salarioAtualD * aumento;
+  print("O seu novo salario é de: $reajuste");
+}
