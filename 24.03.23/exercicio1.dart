5304e2feb52e0770f1c4854c930071c29a0ceb64
Aluno <kaua.moreno2005@gmail.com>
2023-03-24 08:23:32 -0300

    exercicio1 do dia 24.03.23

---
+++ 24.03.23/exercicio1.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+void main() {
+  print("Digite quantas tabuadas deseja: ");
+  var qtd = stdin.readLineSync();
+  int qtdInt = int.parse(qtd!);
+
+  for (int x = 1; x <= qtdInt; x++) {
+    print("\nTabuada: $x/$qtd");
+
+    print("Digite a tabuada desejada: ");
+    var tabDesej = stdin.readLineSync();
+    int tabDesejInt = int.parse(tabDesej!);
+
+    print("Digite até quanto multiplicar");
+    var ateQuant = stdin.readLineSync();
+    int ateQuantInt = int.parse(ateQuant!);
+
+    for(int i = 1; i <= ateQuantInt; i++){
+      int resultado = tabDesejInt * i;
+      print("$tabDesejInt x $i = $resultado");
+    }
+  }
+
+  print("\nPrograma Finalizado.............");
+}
